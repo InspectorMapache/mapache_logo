ab296b549877748cf65d1ec99e65c86252792a91
InspectorMapache <83667528+InspectorMapache@users.noreply.github.com>
2021-06-12 18:37:46 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731rab-63ca-442c-a67b-abc70f28fa5'
+autor 'InspectorMapache'
+
+files({
+	'html/index.html',
+	'html/logo.png'
+  })
+
+client_scripts {
+	'client.lua'
+}
+
+ui_page('mapache_logo/html/index.html')
