5646ff8732c5b31920c1342b8b8736a13654f01b
InspectorMapache <83667528+InspectorMapache@users.noreply.github.com>
2021-11-21 22:57:20 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+autor 'InspectorMapache'
+game 'gta5'
+description 'Create a logo watermark'
+
+files({
+	'html/index.html',
+	'html/logo.png'
+  })
+
+client_scripts {
+	'client.lua'
+}
+
+ui_page({'html/index.html'})
